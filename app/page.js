d0773692f993c7254094ff18878468fab394a788
sagardev09 <sagardbs8121@gmail.com>
2024-02-24 20:51:28 +0530

    footer completed

--- app/page.js
+++ app/page.js
@@ -3,16 +3,18 @@ import { HeroParallax } from "./components/Hero";
 import { Bio } from "./components/Bio";
 import Projects from "../app/components/Projects"
 import { Skills } from "./components/Skills"
+import { Footer } from "./components/Footer"
 
 
 
 export default function Home() {
   return (
-    <main>
+    <main className="">
       <HeroParallax />
       <Bio />
       <Projects />
       <Skills />
+      <Footer />
     </main>
   );
 }
